5ddf1434b4eb85b5841a16b64c620e1d7c611e6c
AleKs <alexanderfranciscog@gmail.com>
2021-10-14 18:07:21 -0400

    Archivos Básicos PHP

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+
+    require 'messages.php';
+
+    define('DB_HOST', '127.0.0.1'); // Servidor de la base de datos.
+    define('DB_USERNAME', 'Alex'); // Usuario de la base de datos.
+    define('DB_PASSWORD', 'alex1234'); // Contraseña de la base de datos. 
+    define('DB_NAME', 'ej_register-mysqli'); // Nombre de la base de datos.
+   
+?>
---
+++ messages.php
@@ -0,0 +1,33 @@
+<?php
+
+ # REGISTER
+    define('FIELDS_MISSING', 'Por favor rellena todos los datos correctamente.');
+
+  // Password
+    define('PASSWORD_NOT_MATCH', 'Las constraseñas no coinciden.');
+    define('SHORT_PASSWORD', 'La contreña debe contener mínimo 8 caracteres.');
+
+  // User    
+    define('USER_ALREADY_EXISTS', 'El usuario que ingresaste ya está en uso.');
+    define('EMAIL_IN_USE', 'El correo que ingresaste ya está en uso.');
+    define('SYMBOLS_NOT_ALLOWED', 'Solo se admiten letras, números, guiones y guiones bajos. Por favor, inténtalo de nuevo sin símbolos.');
+
+  // Error
+    define('USER_REGISTRATION_FAIL', 'Registro de usuario fallido.');
+
+  // Successful      
+    define('USER_REGISTRATION_SUCCESS', 'Registro de usuario se ha realizado correctamente.');
+
+
+ # LOGIN
+    define('LOGIN_FIELDS_MISSING', 'Escriba su correo electrónico y su contraseña.');
+    define('LOGIN_FAIL','Correo electrónico y/o Contraseña no coinciden.');
+
+ # PASSWORD CHANGES
+    define('PASSWORD_CHANGE_SUCCESS', 'Contraseña cambiada con éxito.');
+
+ # FILES
+    define('ERROR_UPLOADING_FILE', 'El archivo no es una imagen o el archivo es muy pesado.');
+    define('MUST_UPLOAD_FILE', 'Debes seleccionar un archivo.');
+
+?>
