4d7bb73242ae4bfbe2e6c95c1e99496bb5e95015
Alexander Pérez <55007470+IamAleKs-0010@users.noreply.github.com>
2021-10-14 18:53:59 -0400

    DB changed

--- config.php
+++ config.php
@@ -5,6 +5,6 @@
     define('DB_HOST', '127.0.0.1'); // Servidor de la base de datos.
     define('DB_USERNAME', 'Alex'); // Usuario de la base de datos.
     define('DB_PASSWORD', 'alex1234'); // Contraseña de la base de datos. 
-    define('DB_NAME', 'ej_register-mysqli'); // Nombre de la base de datos.
+    define('DB_NAME', 'login_singup'); // Nombre de la base de datos.
    
 ?>
