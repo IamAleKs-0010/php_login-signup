d69dd2486b58dd116e66ab827d3f3e30f9ae60c8
AleKs <alexanderfranciscog@gmail.com>
2021-10-15 12:17:17 -0400

    db fixed

--- db.php
+++ db.php
@@ -4,8 +4,8 @@
 
     $conn = mysqli_connect(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME);
 
-    if($conn -> connect_error){
-        die('Error: ' . $conn -> connect_error);
+    if(!$conn){
+        echo 'Error: ' . mysqli_connect_error() . PHP_EOL;
     }
-
+    
 ?>
