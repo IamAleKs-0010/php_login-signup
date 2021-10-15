18f64c7d68e777c22189a907f89d2f06c6968b6c
AleKs <alexanderfranciscog@gmail.com>
2021-10-15 16:36:52 -0400

    New messages

--- messages.php
+++ messages.php
@@ -8,16 +8,15 @@
     define('SHORT_PASSWORD', 'La contreña debe contener mínimo 8 caracteres.');
 
   // User    
-    define('USER_ALREADY_EXISTS', 'El usuario que ingresaste ya está en uso.');
-    define('EMAIL_IN_USE', 'El correo que ingresaste ya está en uso.');
+    define('USER_AND_EMAIL_ALREADY_EXISTS', 'El usuario y/o correo electrónico ya está en uso.');
     define('SYMBOLS_NOT_ALLOWED', 'Solo se admiten letras, números, guiones y guiones bajos. Por favor, inténtalo de nuevo sin símbolos.');
 
   // Email
     define('INVALID_EMAIL', 'Por favor ingresa un correo válido.');
-
+    
   // Error
     define('USER_REGISTRATION_FAIL', 'Registro de usuario fallido.');
-
+    
   // Successful      
     define('USER_REGISTRATION_SUCCESS', 'Registro de usuario se ha realizado correctamente.');
 
@@ -26,8 +25,10 @@
     define('LOGIN_FIELDS_MISSING', 'Escriba su correo electrónico y su contraseña.');
     define('LOGIN_FAIL','Correo electrónico y/o Contraseña no coinciden.');
 
+
  # PASSWORD CHANGES
     define('PASSWORD_CHANGE_SUCCESS', 'Contraseña cambiada con éxito.');
+
 
  # FILES
     define('ERROR_UPLOADING_FILE', 'El archivo no es una imagen o el archivo es muy pesado.');
