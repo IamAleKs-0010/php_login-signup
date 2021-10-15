c3d4d71b4c63b0bab417eaace27618534c2c9eb3
AleKs <alexanderfranciscog@gmail.com>
2021-10-15 13:38:35 -0400

    More Validations

--- register.php
+++ register.php
@@ -13,7 +13,22 @@
         $cpassword = esc($_POST['password_confirm']);
         $reg_date = date("Y-m-d H:i:s");
         
+        if(preg_match('/\s/', $user)){
+            $error .= '<li>' . SYMBOLS_NOT_ALLOWED .'</li>';
+        }
+
+        if(!preg_match('/^[\w\-]+@[\w\-]+.[\w\-]+$/', $email)){
+            $error .= '<li>' . INVALID_EMAIL  . '</li>';
+        }
+
+        if(empty($user) || empty($email) || empty($password) || empty($cpassword)){
+            $error .= '<li>' . FIELDS_MISSING . '</li>';
+        } else{
+
+        }
     }
+
+
 
     require 'view/register.view.php';
 
