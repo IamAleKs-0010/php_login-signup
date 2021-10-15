7aacefb3c1e0ec4f3f058eb38f9b07761bde88f4
AleKs <alexanderfranciscog@gmail.com>
2021-10-14 21:02:11 -0400

    View HTML
    register.view.php
    login.view.php
    content.view.php
    
    These files contain the HTML code that the user will see.

---
+++ view/content.view.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contenido</title>
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;500;800&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+    <link rel="stylesheet" href="css/styles.css">
+</head>
+<body>
+    <div class="container">
+        <div class="content-div">
+            <h1 class="title">Contenido</h1>
+            <p class="text">Contenido que solo puede ver usuarios registrados en la base de datos.</p>
+            <img src="descarga.jfif" alt="image" class="image">
+        </div>
+    </div>
+</body>
+</html>
--- view/login.view.php
+++ view/login.view.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Registrate</title>
+    <title>Iniciar Sesión</title>
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;500;800&display=swap" rel="stylesheet">
@@ -14,22 +14,19 @@
 <body>
     <div class="container">
         <div class="form-group">
-            <h1 class="title">Regístrate</h1>
+            <h1 class="title">Iniciar Sesión</h1>
             <form method="POST" action="#" class="form" enctype="multipart/form-data">
             <!-- Username -->
                 <label for="user"><input type="text" name="user" id="user" autocomplete="off" placeholder="Usuario" required class="btn-required"></label>
 
-            <!-- Email -->
-                <label for="email"><input type="email" name="email" id="email" autocomplete="off" placeholder="Email" required></label>
-
             <!-- Password -->
                 <label for="password"><input type="password" name="password" id="password" autocomplete="off" placeholder="Contraseña" required></label>
 
-            <!-- Password Confirm -->
-                <label: for="passwordConfirm"><input type="password" name="password_confirm" id="passwordConfirm" autocomplete="off" placeholder="Confirmar contraseña" required></label>
 
             <!-- Submit -->
-                <input type="submit" class="btn-submit" value="Registrarme">
+                <input type="submit" class="btn-submit" value="Iniciar Sesión">
+                <p class="subtext">¿No tienes cuenta? <a href="" class="form--link">Regístrate Aquí</a>.</p>
+
             </form>
         </div>
     </div>
