270f264eecd88fea0b1841f6979320d3277f5c11
AleKs <alexanderfranciscog@gmail.com>
2021-10-15 15:53:39 -0400

    DB connection changed to PDO

--- db.php
+++ db.php
@@ -4,8 +4,12 @@
 
     $conn = mysqli_connect(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME);
 
-    if(!$conn){
-        echo 'Error: ' . mysqli_connect_error() . PHP_EOL;
+    try{
+        $host_dbname = "mysql:host=".DB_HOST.";dbname=".DB_NAME;
+        $conn = new PDO($host_dbname, DB_USERNAME, DB_PASSWORD);
+
+    } catch(PDOException $e){
+        die('No se pudo conectar con la base de datos: ' . $e -> getMessage());
     }
     
 ?>
