4dfe24348e0045c1fcbb9acde14e5cbd6c7a1129
AleKs <alexanderfranciscog@gmail.com>
2021-10-15 22:42:34 -0400

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,5 @@
 <?php
 
-
     define('DB_HOST', '127.0.0.1'); // Servidor de la base de datos.
     define('DB_USERNAME', 'Alex'); // Usuario de la base de datos.
     define('DB_PASSWORD', 'alex1234'); // Contraseña de la base de datos. 
