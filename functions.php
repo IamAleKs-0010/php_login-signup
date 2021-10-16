fc5c4090a8fc8f64ba265f00b99347e209e4e131
AleKs <alexanderfranciscog@gmail.com>
2021-10-15 22:30:50 -0400

    Comments added

--- functions.php
+++ functions.php
@@ -1,15 +1,18 @@
 <?php
 
+ # Existe sesión
     function check_session(){
         if(isset($_SESSION['user'])){
             header('Location: index.php');
         }
     }
 
+ # Esc Inputs
     function esc($field){
         return trim(filter_var(stripslashes($field), FILTER_SANITIZE_STRING)); 
     }
 
+ # Esc Email
     function escEmail($field){
         return trim(filter_var(strtolower($field), FILTER_SANITIZE_EMAIL, FILTER_VALIDATE_EMAIL));
     }
