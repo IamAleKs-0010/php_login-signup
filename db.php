454247b1ebc7df47ca796caafd0e8df8b763cba9
AleKs <alexanderfranciscog@gmail.com>
2021-10-15 22:33:40 -0400

    header() to error.html

--- db.php
+++ db.php
@@ -2,14 +2,13 @@
 
     require 'autoload.php';
 
-    $conn = mysqli_connect(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME);
-
     try{
         $host_dbname = "mysql:host=".DB_HOST.";dbname=".DB_NAME;
         $conn = new PDO($host_dbname, DB_USERNAME, DB_PASSWORD);
 
     } catch(PDOException $e){
-        die('No se pudo conectar con la base de datos: ' . $e -> getMessage());
+        header('Location: error.html');
+        die();
     }
     
 ?>
