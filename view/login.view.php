49e77b84dccae67b3abf74ff2f57f078e34610ba
AleKs <alexanderfranciscog@gmail.com>
2021-10-19 14:18:18 -0400

    Font Styles changed to: 500, 600, 800

--- view/login.view.php
+++ view/login.view.php
@@ -7,7 +7,7 @@
     <title>Iniciar Sesión</title>
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;500;800&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,500;0,600;0,800;1,600&display=swap" rel="stylesheet">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
     <link rel="stylesheet" href="css/styles.css">
 </head>
