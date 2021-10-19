182660530067e94ca9beda92effc3fbac28f0a47
AleKs <alexanderfranciscog@gmail.com>
2021-10-19 18:32:55 -0400

    From 'error.html' to 'error.view.php'

--- view/error.view.php
+++ view/error.view.php
@@ -9,15 +9,15 @@
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,500;0,600;0,800;1,600&display=swap" rel="stylesheet">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
-    <link rel="stylesheet" href="css/styles.css">
+    <link rel="stylesheet" href="../css/main.css">
 </head>
 </head>
 <body>
     <div class="container">
         <div class="error-div">
             <h1 class="error-title">Oops... Ha habido un error con el servidor.</h1>
-            <div class="come-back-div">
-                <p class="come-back-text"><a href="index.php" class="come-back-link">Volver</a></p>
+            <div class="goback-div">
+                <p><a href="../index.php" class="goback-link">Volver</a></p>
             </div>
         </div>
     </div>
