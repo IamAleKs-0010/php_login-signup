935ced48d35932a33372380b81aefa2787d4e57e
AleKs <alexanderfranciscog@gmail.com>
2021-10-23 00:27:30 -0400

    More Validations

--- passotp.php
+++ passotp.php
@@ -5,6 +5,8 @@
 
     check_session();
 
+    $error = '';
+
     if(isset($_GET['email']) && isset($_GET['token'])){
         $email = $_GET['email'];
         $hiddenEmail = emailAsterisk($email);
@@ -16,9 +18,27 @@
             array_push($codes, $code); // Add all the codes to the array '$codes'
             $recoveryCode = $codes[0][0] . $codes[0][1] . $codes[0][2] . $codes[0][3];
 
+
+            if(strlen($recoveryCode) < 4){
+                $error .= '<li>' . INVALID_CODE . '</li>';
+            } else{
+                $query = 'SELECT code FROM passwords WHERE code = :code';
+                $stmt = $conn -> prepare($query);
+                $stmt -> execute( array(':code' => $recoveryCode) );
+
+                $check = $stmt -> fetch();
+
+                if($check){
+                    // Re direct
+                } else {
+                    $error .= '<li>' . INCORRECT_CODE . '</li>';
+                }
+            }
         }
     } else{
         header('Location: login.php');
     }
+
+    require 'view/passotp.view.php'
 
 ?>
